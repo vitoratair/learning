59c32f33655444aca3146b82b11d06643ab832ea
Vitor Atair <vitor.ruts@gmail.com>
2014-03-14 09:54:40 -0300

    adicionado hello world

---
+++ python/conteudo/hello_world.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+#	Hello World da linguagem
+
+
+def main():
+
+	# Pula duas linhas
+    print "\n\n"
+
+    # Imprimi no terminal padrão a string Hello World!
+    print "Hello World!"
+
+    # Pula duas linhas
+    print "\n\n"
+
+
+if "__main__" == __name__:
+    main()
